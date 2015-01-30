fdced6b8413d2eb8fce141f77a9c751825bc1870
Wesley Massuda <wesley.massuda@gmail.com>
2015-01-30 00:32:18 -0200

    in place refactor of grid diagram renderer

--- Test.hs
+++ Test.hs
@@ -230,51 +230,44 @@ renderGrid env l  r (Right (s,(n,Tee (TeeConfig tc@[rl,b,rr] _ _ _ _)) ))
         (visited,_,_) <- get
         if S.member i visited then return mempty else renderGrid env n ri (var i env)
 
-
 renderGrid env n r (Left (l,h,t,e))
   | n == h =  do
-    (visited,visitedNode,nodeMap) <- get
-    let dist =  translate ( p2r ( rotateBy (r + snd (var n nodeMap)) ( L.foldr (offsetR) (p2 (0,0)) e))) (fst $ var n nodeMap)
-    g <- nextNode (angleR e) dist t
-    return $  rotateBy r $ foldr (renderLinkR ) g (e)
+    path t e
   | n == t = do
-    (visited,visitedNode,nodeMap) <- get
-    let dist =  translate ( p2r ( rotateBy (r + snd (var n nodeMap))  ( L.foldr (offsetR) (p2 (0,0)) (revElems e)))) ( fst $ var n nodeMap)
-    g <- nextNode (angleL e) dist h
-    return $ rotateBy r $ foldr (renderLinkR ) g (revElems e)
+    path h (revElems e)
   where
+      path h e = do
+          (visited,visitedNode,nodeMap) <- get
+          let dist =  translate (p2r ( rotateBy (r + snd (var n nodeMap)) ( L.foldr offset (p2 (0,0)) e))) (fst $ var n nodeMap)
+          g <- nextNode (angle e) dist h
+          return $  rotateBy r $ foldr renderLink  g e
       nextNode  a dist h = do
           (visited,visitedNode,nodeMap) <- get
           visitLink l
           if  not $ S.member h visitedNode
             then do
-              markNode h (dist,traceShow (r,a,snd $var n nodeMap) $ r + a + (snd $ var n nodeMap) )
+              markNode h (dist, r + a + (snd $ var n nodeMap) )
               renderGrid env l 0 (var h env)
             else
                 if  abs (distance dist  (fst $ var h nodeMap)) < 1e-2
                  then {-traceShow (show l <> " exact union point " <> show h <> " " <> show dist <> " == " <>  show (var h nodeMap))  $-}return mempty
                  else traceShow (show l <> " non exact union point " <> show h <> " " <> show dist <> " /= " <>  show (var h nodeMap)) $ return errorCross
 
-      renderLinkR t@(Tubo _ c _) o =  offsetR t  o <> (fromOffsets [realToFrac c * unitX ] # lwL 0.04<> translate (r2 (c/2,0.12)) ( text (show l)) # fontSizeL 0.2 # fc black )
-      renderLinkR j@(Joelho _ _ _ _ ) l = joelho <> offsetR j  l
-      renderLinkR i j = j
-      {-renderLinkL t@(Tubo _ c _) o =  offsetL t o <> (fromOffsets [-realToFrac c * unitX ]  # lwL 0.04 # lc purple<> translate (r2 (-c/2,-0.12)) ( text (show l)) # fontSizeL 0.2 # fc black )
-      renderLinkL j@(Joelho _ _ _ _ ) l = joelho <> offsetL j l
-      renderLinkL i j = offsetL i j
-      offsetL (Tubo _ c _) o =  translate (r2 (c,0)) o
-      offsetL (Joelho _ _ DRight  _ ) l = rotateBy (1/4) l
-      offsetL (Joelho _ _ DLeft _ ) l = rotateBy (-1/4) l
-      offsetL i j = j-}
-      offsetR (Tubo _ c _) o =  translate (r2 (c,0)) o
-      offsetR (Joelho _ _ DRight  _ ) l = rotateBy (1/4) l
-      offsetR (Joelho _ _ DLeft _ ) l = rotateBy (-1/4) l
-      offsetR i j = j
-      angle (Joelho _ _ DRight _ ) = 1/4
-      angle (Joelho _ _ DLeft _ ) = -1/4
-      angle  i = 0
-      angleR = sum . fmap angle
-      angleL = sum . fmap negate . fmap angle
-      joelho = circle 0.1 # fc blue # lwL 0.04
+      renderLink t@(Tubo _ c _) o =  offset t  o <> (line <> label)
+        where label = translate (r2 (c/2,0.12)) ( text (show l)) # fontSizeL 0.2 # fc black
+              line = fromOffsets [realToFrac c * unitX ] # lwL 0.04
+      renderLink j@(Joelho _ _ _ _ ) l = joelho <> offset j  l
+       where joelho = circle 0.1 # fc blue # lwL 0.04
+      renderLink i j = j
+      offset (Tubo _ c _) o =  translate (r2 (c,0)) o
+      offset (Joelho _ _ DRight  _ ) l = rotateBy (1/4) l
+      offset (Joelho _ _ DLeft _ ) l = rotateBy (-1/4) l
+      offset i j = j
+      angle = sum . fmap angle
+        where
+          angle (Joelho _ _ DRight _ ) = 1/4
+          angle (Joelho _ _ DLeft _ ) = -1/4
+          angle  i = 0
 
 renderGrid i j r l = error $ show l
 revElem (Joelho i j DRight k)  =  (Joelho i j DLeft k)
