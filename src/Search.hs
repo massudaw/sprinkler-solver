ebbb7d0b68c0c525a897e3561f067c90d09870c5
Wesley Massuda <wesley@capital-match.com>
2020-07-21 23:04:34 +0800

    Add module search

---
+++ src/Search.hs
@@ -0,0 +1,15 @@
+module Search (connected) where
+import qualified Data.Set as S
+import Data.Graph
+import qualified Data.Array as A
+
+-- Find if two points are connecting using the neighbors list
+connected :: A.Ix a => a -> a -> A.Array a [a] -> [[(a, a)]]
+connected x y g = helper x y g (S.singleton x)
+  where
+    helper a b g visited
+        | a == b    = [[]]
+        | otherwise = [(a,c):path | c <- next ,  path <- helper c b g nextS]
+      where
+        next = filter (not . (`S.member` visited))  (g A.! a)
+        nextS = foldr S.insert visited  next
